c5497fdc31df46500767b9b68334c4914fa6de11
MilesNaismith <truskov@inbox.ru>
2018-10-05 10:43:17 +0300

    collect.py added

--- collect.py
+++ collect.py
@@ -3,7 +3,7 @@ import metro_parser
 import perekrestok_parser
 import db_from_csv
 
-#auchan_parcer.main()
-#metro_parser.main()
-#perekrestok_parser.main()
+auchan_parcer.main()
+metro_parser.main()
+perekrestok_parser.main()
 db_from_csv.main()
